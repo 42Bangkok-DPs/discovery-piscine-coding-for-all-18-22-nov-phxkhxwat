41031a20dc80ce3eebb3119a55c159cd7d359cfb
Phakhawat Tae. <90884513+phxkhxwat@users.noreply.github.com>
2024-11-19 08:18:04 +0000

    first

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,16 @@
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+result = x*y 
+
+
+print(x , "x" , y , "=" ,result)
+
+if result > 0 :
+    print("The result is positive")
+
+elif result < 0 :
+    print("The result is negative.")
+
+else:
+    print("The result is zero.")
