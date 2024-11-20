29b1d437b5eec501ffa491b8e4bad463f87bfd5b
Phakhawat Tae. <90884513+phxkhxwat@users.noreply.github.com>
2024-11-20 06:54:37 +0000

    edit1

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) > 1:
+    input_value = sys.argv[1]
+
+    if input_value == "yolo" :
+        print("none")
+
+    else :
+        print("Invalid Input")
+            
+
+else :
+    row = 0
+    while row <= 10 :
+        colume = 1 
+        print("Table de",row,":",end = " ")
+        while colume <= 10 :
+            print(colume*row , end = " ")
+            colume += 1
+        print()
+        row += 1
