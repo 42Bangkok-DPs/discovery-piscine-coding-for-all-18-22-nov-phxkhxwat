279f5b16d9d362f839b956bd10e0e4b7affc6f9d
Phakhawat Tae. <90884513+phxkhxwat@users.noreply.github.com>
2024-11-20 09:21:52 +0000

    first

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+x = input("Give me the first number: ")
+y = input("Give me the second number: ")
+
+x = int(x)
+y = int(y)
+
+print("Thank you!")
+
+print(x ,"+",y,"=",x+y)
+print(x ,"-",y,"=",x-y)
+print(x ,"/",y,"=",x/y)
+print(x ,"*",y,"=",x*y)
