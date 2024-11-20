27c677a3069f511a3f27c7460275ae714c8e2e19
Phakhawat Tae. <90884513+phxkhxwat@users.noreply.github.com>
2024-11-20 09:15:19 +0000

    first

---
+++ cell04/ex01/age.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+x = input("Please tell me you age: ")
+age = int(x)
+
+print("You are currently", age , "years old.")
+
+for i in range(10,40,10):
+    print("In ", i , "years , you'll be " , age+i ,"years old.")
